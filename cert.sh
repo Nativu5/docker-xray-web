941d0e3e95cc78de91c1f92a24a0006a9d983447
Nativu5 <sdtalyk@163.com>
2021-02-24 17:40:51 +0800

    remove cert.sh

--- cert.sh
+++
@@ -1,9 +0,0 @@
-sudo setcap 'cap_net_bind_service=+ep' /usr/bin/socat
-
-~/.acme.sh/acme.sh --issue --standalone -d yourdomain.com -keylength ec-256 --force
-
-~/.acme.sh/acme.sh --install-cert -d yourdomain.com --ecc \
-            --fullchain-file /path/to/cert/xray.crt \
-            --key-file /path/to/cert/xray.key
-
-chmod +r /path/to/cert/xray.key
